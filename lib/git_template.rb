f08d5087e9fa7d20459ab70c89871e7d487457f3
Mateusz Wieczorek <mateusz.wu@gmail.com>
2023-10-12 14:20:39 +0200

    test

---
+++ lib/git_template.rb
@@ -0,0 +1,21 @@
+class ExampleCommand
+  def initialize(context)
+    @context = context
+  end
+
+  def method_missing(method_name, *args, &block)
+    if @context.respond_to?(method_name, true)
+      @context.send(method_name, *args, &block)
+    else
+      super
+    end
+  end
+
+  def call
+    send(:after_bundle) do
+      git(:init)
+      git(add: '.')
+      git(commit: %Q{ -m "Initial commit" })
+    end
+  end
+end
---
+++ main_template.rb
@@ -0,0 +1,4 @@
+require_relative './lib/git_template.rb'
+
+ExampleCommand.new(self).call
+
