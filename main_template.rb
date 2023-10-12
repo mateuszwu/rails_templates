c938a6fcb5f938f41786e9503ed9e6565156ffb1
Mateusz Wieczorek <mateusz.wu@gmail.com>
2023-10-12 14:50:42 +0200

    asdfasdf

--- main_template.rb
+++ main_template.rb
@@ -1,4 +1,29 @@
-require_relative './lib/git_template.rb'
+ENV = :remote
+
+HOST = 'https://raw.githubusercontent.com/mateuszwu/rails_templates/main'
+FILES = [
+  'lib/git_template.rb'
+]
+
+if ENV == :local
+  FILES.each do |file|
+    require_relative file
+  end
+else
+  require 'net/http'
+  require 'open-uri'
+
+  FILES.each do |file|
+    url = URI.parse("#{HOST}/#{file}")
+    response = Net::HTTP.get_response(url)
+
+    if response.code == '200'
+      code = response.body
+      eval code
+    else
+      puts "Failed to fetch the remote script (HTTP status code: #{response.code})"
+    end
+  end
+end
 
 ExampleCommand.new(self).call
-
