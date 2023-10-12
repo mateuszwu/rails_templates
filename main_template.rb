997670a2f2a4e2b274c234cd52ee3f9ca375c11a
Mateusz Wieczorek <mateusz.wu@gmail.com>
2023-10-12 14:33:02 +0200

    test

--- main_template.rb
+++ main_template.rb
@@ -1,4 +1,10 @@
-require_relative './lib/git_template.rb'
+# require_relative './lib/git_template.rb'
 
-ExampleCommand.new(self).call
+# ExampleCommand.new(self).call
 
+
+after_bundle do
+  git(:init)
+  git(add: '.')
+  git(commit: %Q{ -m "Initial commit" })
+end
